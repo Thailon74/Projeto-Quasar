8bdfa169733689b7a950f8a6d143bbf237ae8402
Thailon <thailon74@gmail.com>
2023-04-17 18:13:25 -0300

    adição Login

---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="login-page">
+    <div class="login-form">
+      <q-input v-model="username" label="CPF" />
+      <br>
+      <q-input v-model="password" label="Senha" type="password" />
+      <q-btn label="Entrar" class="q-mt-md" id="btn-login" @click="login" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      //autenticacao
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.login-form {
+  width: 400px;
+  padding: 30px;
+  background-color: #fff;
+}
+
+#btn-login{
+  background-color: white;
+}
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {path: "/login", component: () => import("components/Login.vue")}
     ]
   },
 
@@ -14,6 +15,7 @@ const routes = [
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
   }
+
 ]
 
 export default routes
